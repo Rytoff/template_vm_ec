f7294cbbcfe943c33e0900d839b868da08313d62
Rytoff <roman.rytoff@gmail.com>
2022-01-27 17:32:54 +0200

    webpack loaders installed

--- webpack.config.js
+++ webpack.config.js
@@ -26,16 +26,34 @@ module.exports = {
       template: 'index.html',
       inject: 'body',
     }),
-    new CopyPlugin({
-      patterns: [
-        {
-          from: path.resolve(__dirname, 'src/favicon.ico'),
-          to: path.resolve(__dirname, 'dist'),
-        },
-      ],
-    }),
+    // new CopyPlugin({
+    //   patterns: [
+    //     {
+    //       from: path.resolve(__dirname, 'src/favicon.ico'),
+    //       to: path.resolve(__dirname, 'dist'),
+    //     },
+    //   ],
+    // }),
     new MiniCssExtractPlugin({
       filename: 'bundle.[hash].css',
     }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
 }
