c29a36aa22dbb00fae4a903b051882764b6118ca
Rytoff <roman.rytoff@gmail.com>
2022-01-28 12:52:27 +0200

    base environment done

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import './module'
 import './scss/index.scss'
 
-console.log('index')
+// console.log('index')
--- src/module.js
+++ src/module.js
@@ -1,10 +1,10 @@
-console.log('module.js')
+// console.log('module.js')
 
 async function start() {
-  const header = document.createElement('div')
-  header.innerHTML = 'header'
-  console.log(header)
-  document.body.append(header)
+  // const header = document.createElement('div')
+  // header.innerHTML = 'header'
+  // console.log(header)
+  // document.body.append(header)
   return await Promise.resolve('async working!')
 }
 
